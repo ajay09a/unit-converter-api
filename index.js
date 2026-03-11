aded10816b76705f5177fa7c239c5539f3385487
Ajay <ajaykaloniya@gmail.com>
2026-03-12 02:00:52 +0530

    add api versioning

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const converterRoutes = require("./src/routes/converterRoutes");
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+app.use("/api/v1", converterRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on Port: ${PORT}`);
