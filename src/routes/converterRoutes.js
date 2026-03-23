44b144a823b10b7965b93a98a594e4a294da0f95
Ajay <ajaykaloniya@gmail.com>
2026-03-23 17:33:19 +0530

    add weight routes

--- src/routes/converterRoutes.js
+++ src/routes/converterRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const converterController = require("../controllers/converterController");
+const lengthController = require("../controllers/converterController");
+const weightController = require("../controllers/weightConverterController")
 
-router.get("/convert/length", converterController.convertLength);
+router.get("/convert/length", lengthController.convertLength);
+router.get("/convert/weight", weightController.convertWeight);
 
 router.get("/", (req,res)=>{
     res.json({
