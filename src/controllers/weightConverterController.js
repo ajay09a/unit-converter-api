fe35dd09433f86e65d4baa1c8cfad5573734335d
Ajay <ajaykaloniya@gmail.com>
2026-03-23 17:30:28 +0530

    add weight converter controller

---
+++ src/controllers/weightConverterController.js
@@ -0,0 +1,30 @@
+const convertWeight = require("../utils/weightConversion");
+
+exports.convertWeight = (req, res, next)=>{
+    const value = Number(req.query.value);
+    const from = req.query.from;
+    const to = req.query.to;
+
+    if(!value || !from ||!to){
+        return next({
+            status: 400,
+            message: "required all field",
+        });
+    }
+
+    const result = convertWeight(value, from, to);
+
+    if(result == null){
+        return next({
+            status: 400,
+            message: "Invalid conversion"
+        });
+    }
+
+    res.json({
+        value: value,
+        from: from,
+        to: to,
+        result: result
+    });
+}
