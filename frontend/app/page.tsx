2ae89ac8ceb38202f31d82cd6c51af8efc2847bf
Ajay <ajaykaloniya@gmail.com>
2026-03-25 19:16:34 +0530

    create basic working page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,13 +4,24 @@ import { useState } from "react";
 
 export default function Page() {
   const [data, setData] = useState<any>(null);
+  const [conversiontype, setConversionType] = useState("length");
+  const [value, setValue] = useState<number | "">("");
+  const [fromUnit, setFromUnit] = useState<string>("");
+  const [toUnit, setToUnit] = useState<string>("");
+
+  const units: Record<string, string[]> = {
+    length: ["mm","cm","meter","km","inch","foot","yard","mile"],
+    weight: ["mg","gram","kg","ounce","pound"],
+    temperature: ["c","f","k"]
+  };
+
 
   const fetchData = async () => {
     try {
       console.log("ENV:", process.env.NEXT_PUBLIC_API_BASE_URL);
 
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/convert/length?value=10&from=inch&to=cm`
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/convert/${conversiontype}?value=${value}&from=${fromUnit}&to=${toUnit}`
       );
 
       const result = await res.json();
@@ -24,11 +35,32 @@ export default function Page() {
 
   return (
     <div>
+        <h1>Unit Converter</h1>
+        <div>
+            <button onClick={()=>setConversionType("length")}>Length</button>
+            <button onClick={()=>setConversionType("weight")}>weight</button>
+            <button onClick={()=>setConversionType("temperature")}>Temperature</button>
+        </div>
+        <label>
+            value:
+            <input type="number" value={value} onChange={e => setValue(Number(e.target.value))} />
+        </label>
+        <label>
+          From: 
+          <select value={fromUnit} onChange={e => setFromUnit(e.target.value)}>
+            <option value="">Select</option>
+            {units[conversiontype].map(u => <option key={u} value={u}>{u}</option>)}
+          </select>
+        </label>
+        <label>
+          To: 
+          <select value={toUnit} onChange={e => setToUnit(e.target.value)}>
+            <option value="">Select</option>
+            {units[conversiontype].map(u => <option key={u} value={u}>{u}</option>)}
+          </select>
+        </label>
+
       <button onClick={fetchData}>Convert</button>
-
-      <h3>ENV: {process.env.NEXT_PUBLIC_API_BASE_URL}</h3>
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
 
       {data && <h2>Result: {data.result}</h2>}
     </div>
